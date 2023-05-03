b11445128860f9ac0431293a0a430b5d39038bf1
Naematullah Rahmatullah <naematrahmatullah@gmail.com>
2023-05-03 17:07:06 -0400

    first commit

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from basic_app import views
+
+app_name = 'basic_app'
+urlpatterns = [
+    url('register/', views.registration, name='registration'),
+    url('user_login/', views.user_login, name='user_login')
+]
